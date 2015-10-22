a4d773c069c11463dd51a4c48ea2aefba1cfef59
Lin Ma <majcjc@gmail.com>
2015-10-22 15:25:28 +0800

    Init commit

---
+++ src/Attributes.h
@@ -0,0 +1,19 @@
+#ifndef LGMESH_ATTRIBUTES_H
+#define LGMESH_ATTRIBUTES_H
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <vector>
+
+namespace LgMesh {
+
+
+
+
+
+
+}
+
+#endif // !LGMESH_ATTRIBUTES_H
---
+++ src/TriMesh.cpp
@@ -0,0 +1,1 @@
+#include "LgMeshTypes.h"
